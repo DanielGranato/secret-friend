fbf6dd8d50b44b8eb1ad0e72261fb63474e6c3e9
Daniel Granato <dgmgranato@gmail.com>
2026-08-11 17:54:56 -0300

    add fisher-yates algorithm

--- js/app.js
+++ js/app.js
@@ -10,6 +10,17 @@ function adicionar () {
 }
 
 function sortear () {
-    
+    embaralha(amigos);
 }
 
+function embaralha(lista) {
+
+    for (let indice = lista.length; indice; indice--) {
+
+        const indiceAleatorio = Math.floor(Math.random() * indice);
+
+        // atribuição via destructuring
+        [lista[indice - 1], lista[indiceAleatorio]] = 
+            [lista[indiceAleatorio], lista[indice - 1]];
+    }
+}
